e3bbf5d751c4144b1855d8e2cb13b27d4ab2e753
Miroslav Ananiev <m.a.ananiev@gmail.com>
2023-07-12 17:54:14 +0300

    Added flyway configuration

--- user-management-app/src/main/java/com/example/usermanagementapp/controller/UserController.java
+++ user-management-app/src/main/java/com/example/usermanagementapp/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.usermanagementapp.controller;
 
 import com.example.usermanagementapp.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "You are home";
     }
 }
--- user-management-app/src/main/java/com/example/usermanagementapp/dto/UserDTO.java
+++ user-management-app/src/main/java/com/example/usermanagementapp/dto/UserDTO.java
@@ -1,4 +1,83 @@
 package com.example.usermanagementapp.dto;
 
+
+
+import java.time.LocalDate;
+
 public class UserDTO {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    private String phoneNumber;
+
+    private String email;
+
+    public UserDTO(long id, String firstName, String lastName, LocalDate dateOfBirth, String phoneNumber, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public UserDTO setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserDTO setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserDTO setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public UserDTO setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public UserDTO setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
 }
